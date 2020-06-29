1eae527ff29a6c7899717eb7727c5f37af696944
strikeraryu <striker.aryu56@gmail.com>
2020-06-29 23:25:39 +0530

    Square Submatrices - Better Solution

--- june_lunchtime_2020_div2_codechef/Square Submatrices.cpp
+++ june_lunchtime_2020_div2_codechef/Square Submatrices.cpp
@@ -1,6 +1,6 @@
 // link of the problem https://www.codechef.com/LTIME85B/problems/SQRSUBMA/
 
-// 50% - SubTask1 passed
+// 100% - all SubTask passed
 
 
 #include <bits/stdc++.h>
@@ -11,28 +11,36 @@ int main() {
     cin>>t;
     while(t--){
         int n, x;
+        long long int cnt=0;
         scanf("%d %d",&n,&x);
         vector<int> v(n);
-        vector<vector<int>> dp(n, vector<int>(n));
-        map<pair<int,int>,int> cnts;
         for(int i=0;i<n;i++)scanf("%d",&v[i]);
-        for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum+=v[j];
-                dp[i][j] = sum;
-                cnts[{sum,j-i+1}]++;
+        vector<int> fcts;
+        for(int i=1;i<=sqrt(x);i++){
+            if(x%i==0){
+                if(x/i!=i) fcts.push_back(x/i);
+                fcts.push_back(i);
             }
         }
-        int cnt = 0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(x%(j-i+1)==0){
-                    int tmp = x/(j-i+1) - dp[i][j];
-                    cnt+=cnts[{tmp,j-i+1}];
+        for(int fct : fcts){
+            int sum = 0;
+            unordered_map<long long int, int> cnts;
+            vector<long long int> subs;
+            for(int i=0;i<n;i++){
+                if(i>fct-1) sum-=v[i-fct];
+                sum+=v[i];
+                if(i>=fct-1 && sum<=x/fct){ 
+                    cnts[sum]++; 
+                    subs.push_back(sum);
                 }
             }
+            for(auto sub : subs){
+                int tmp = x/fct - sub;
+                if(cnts.count(tmp)!=0)
+                cnt+=cnts[tmp];
+            }
         }
+        
         cout<<cnt<<endl;
     }
     return 0;
